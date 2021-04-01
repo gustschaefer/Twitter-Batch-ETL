22779641f15a10e9e2b1a9178e81eaa57f36685d
Gustavo Schaefer <gstvo.schaefer@gmail.com>
2021-04-01 13:53:54 -0300

    test parque

--- json_to_parquet.py
+++ json_to_parquet.py
@@ -5,7 +5,7 @@ from pyspark.sql.types import *
 
 sc = SparkSession.builder.appName("JsontoParquet").getOrCreate()
 
-df = sc.read.json('tweet-data/json/TweetsData-Brazil-2021-03-29.json')
-#df.write.parquet('tweet-data/parquet/t.parquet')
+df = sc.read.option("multiline","true").json('test.json')
+df.write.parquet('t.parquet')
 
-context.stop()
+sc.stop()
