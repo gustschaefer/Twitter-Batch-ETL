6f8dc4ac7093631e2060d2557c0f6126df1ed888
Gustavo Schaefer <gstvo.schaefer@gmail.com>
2021-04-01 17:16:18 -0300

    json2parquet

--- json_to_parquet.py
+++ json_to_parquet.py
@@ -1,11 +1,23 @@
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 from pyspark.sql.functions import *
-from pyspark.sql.types import *
+import os
 
 sc = SparkSession.builder.appName("JsontoParquet").getOrCreate()
 
-df = sc.read.option("multiline","true").json('test.json')
-df.write.parquet('t.parquet')
+# paths
+json_input_data_folder = "tweets-data/json"
+parquet_output_data_folder = "tweets-data/parquet"
 
+def json_to_parquet(json_input_data_folder, parquet_output_data_folder):
+
+    for file in os.listdir(json_input_data_folder):
+        if file.endswith(".json"):
+            file_name = os.path.splitext(file)[0]
+            path_json = os.path.join(json_input_data_folder, file)
+            path_parquet = os.path.join(parquet_output_data_folder, file_name)
+            df = sc.read.option("multiline","true").json(path_json)
+            df.write.parquet(path_parquet)
+
+json_to_parquet(json_input_data_folder, parquet_output_data_folder)
 sc.stop()
