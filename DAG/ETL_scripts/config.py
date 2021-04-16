453e1ee7c537d61556b9baa31a8f4ce8200f793c
Gustavo Schaefer <gstvo.schaefer@gmail.com>
2021-04-15 23:42:52 -0300

    config

--- DAG/ETL_scripts/config.py
+++ DAG/ETL_scripts/config.py
@@ -1,12 +1,12 @@
 # Tweepy API credentials
-consumer_key = "bj1HxgkRGzDZTqKZbd5bZxsCT"
-consumer_secret = "rgouf15ueg5Q6Jc0Z1JpUpoYYghs8GZITVs7pZmSw9Dvcto60B"
-access_token = "1376541401396224000-W9K7oeHJ4QWZPilBYKkK7ued1QlJbe"
-access_token_secret = "TyiBFblzpsNsV0r3FL14OHRYbq7XIdatrzQyPBuWsh3jd"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 # temp files RELATIVE path
 temp_json_files = "./temp/json/"
 temp_parquet_files = "./temp/parquet/"
 
 # S3 
-bucket_name = "twitter-trending-topics-etl"
+bucket_name = "SEU_BUCKET_NAME"
